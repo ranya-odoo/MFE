555f03f2cc81ffb23dd0b5a55ab6dc83ad38e3a1
RanyaBej <bejaoui.ranyarania@gmail.com>
2025-04-15 12:35:28 +0100

    first commit

---
+++ smq/wizards/document_rejection_wizard.py
@@ -0,0 +1,19 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+class DocumentRejectionWizard(models.TransientModel):
+    _name = 'document.rejection.wizard'
+    _description = "Assistant de rejet"
+
+    documentation_id = fields.Many2one('documentation', string="Document concerné")
+    raison = fields.Text(string="Raison du rejet", required=True)
+
+    def action_confirmer_rejet(self):
+        if not self.raison:
+            raise UserError("Veuillez indiquer la raison du rejet.")
+        self.documentation_id.write({
+            'state': 'rejected',
+            'raison_rejet':self.raison,
+            'rejected':True,
+        })
+        # Tu peux aussi logger, envoyer un message, etc.
